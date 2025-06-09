d21a86ea96d4c82d1989542f07720b1f0cbe74dd
Mahmud Hasan Sizan <mahmudhasan3801@gmail.cm>
2025-06-10 00:49:53 +0600

    tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero"
+
+
+function App() {
+ 
+
+  return (
+    <main className="relative min-h-screen overflow-x-hidden w-screen">
+     <Hero />
+    </main>
+  )
+}
+
+export default App
